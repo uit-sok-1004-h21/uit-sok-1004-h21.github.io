8f33b758bcf50874ada9c11f760273a08d539705
Oystein Myrland <omy000@uit.no>
2021-08-26 13:03:33 +0200

    Add files via upload

---
+++ ovingsoppgaver_1.R
@@ -0,0 +1,30 @@
+#' Øvingsoppgaver
+#' Bruk CO2 dataene fra forelesning 2
+
+
+#' Oppgave 1
+#' Lag et plot der "year" er på x-aksen, "co2_per_capita" er på y-aksen
+#' for landene Norge, Sverige, Danmark, Finland og Island
+#' Drøft dine funn.
+
+
+#' Oppgave 2
+#' Modifiser plottet i Oppgave 1 til bare å gjelde årene fra og med 1970.
+#' Drøft dine funn.
+
+
+#' Oppgave 3
+#' Modifiser plottet i Oppgave 1 til bare å gjelde årene fra og med 1900 til og med 1950.
+#' Drøft dine funn.
+
+
+#' Oppgave 4
+#' Lag et plot der "year" er på x-aksen, "co2_norden" er på y-aksen, men der "co2_norden" er sum av co2
+#' for landene Norge, Sverige, Danmark, Finland og Island
+#' Drøft dine funn.
+
+
+#' Oppgave 5
+#' I forhold til funnene i Oppgave 4, hvilket av de nordiske landene bidrar mest til de nordiske utslippene av co2 etter 1980?
+#' Drøft dine funn.
+
